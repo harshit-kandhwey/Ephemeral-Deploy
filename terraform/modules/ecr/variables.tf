b23922d525c6e78bdaf0284fb1d85a7521d66e9b
Harshit Kandhwey <harshitkandhwey@gmail.com>
2026-03-29 23:18:29 +0530

    Added Outputs and Variable file in ECR Module

---
+++ terraform/modules/ecr/variables.tf
@@ -0,0 +1,14 @@
+variable "project" {
+  description = "Project name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment (dev, staging, prod)"
+  type        = string
+}
+
+variable "common_tags" {
+  description = "Common tags for all resources"
+  type        = map(string)
+}
